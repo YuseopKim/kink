4efd3af1e70031bfdaa10246b222727309cc0d18
YuseopKim <87454195+YuseopKim@users.noreply.github.com>
2022-05-23 21:41:46 +0900

    Update entrypoint-wrapper.sh

--- docker-images/kind-cluster/node/entrypoint-wrapper.sh
+++ docker-images/kind-cluster/node/entrypoint-wrapper.sh
@@ -11,24 +11,24 @@ CGROUP_SUBSYSTEMS=$(findmnt -lun -o source,target -t cgroup | grep "${CURRENT_CG
 
 mount --make-rprivate /sys/fs/cgroup
 
-#echo "${CGROUP_SUBSYSTEMS}" |
-#while IFS= read -r SUBSYSTEM; do
+echo "${CGROUP_SUBSYSTEMS}" |
+while IFS= read -r SUBSYSTEM; do
   # This is because we set Kubelet's cgroup-root to `/kubelet` by
   # default. We have to do that because otherwise, it'll collide
   # with the cgroups used by the Kubelet running on the host if we
   # run Konvoy docker cluster within a Kubernetes pod, resulting
   # random processes to be killed.
   mkdir -p "${SUBSYSTEM}/kubelet"
-  #if [ "${SUBSYSTEM}" == "/sys/fs/cgroup/cpuset" ]; then
+  if [ "${SUBSYSTEM}" == "/sys/fs/cgroup/cpuset" ]; then
     # This is needed. Otherwise, assigning process to the cgroup
     # (or any nested cgroup) would result in ENOSPC.
-  #  cat "${SUBSYSTEM}/cpuset.cpus" > "${SUBSYSTEM}/kubelet/cpuset.cpus"
-  #  cat "${SUBSYSTEM}/cpuset.mems" > "${SUBSYSTEM}/kubelet/cpuset.mems"
-  #fi
+    cat "${SUBSYSTEM}/cpuset.cpus" > "${SUBSYSTEM}/kubelet/cpuset.cpus"
+    cat "${SUBSYSTEM}/cpuset.mems" > "${SUBSYSTEM}/kubelet/cpuset.mems"
+  fi
   # We need to perform a self bind mount here because otherwise,
   # systemd might delete the cgroup unintentionally before the
   # kubelet starts.
-  #mount --bind "${SUBSYSTEM}/kubelet" "${SUBSYSTEM}/kubelet"
-#done
+  mount --bind "${SUBSYSTEM}/kubelet" "${SUBSYSTEM}/kubelet"
+done
 
 exec "$@"
